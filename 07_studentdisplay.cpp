0f725f541b7257953e112a3d05130707d08b892e
neelsuryawanshi <neelsuryavanshi04@gmail.com>
2026-09-23 11:28:29 +0530

    Add student roll number display program

---
+++ 07_studentdisplay.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int roll1,roll2,roll3,roll4,roll5;
+
+    cout<<"Enter student 1 roll number: ";
+    cin>>roll1;
+    cout<<"Enter student 2 roll number: ";
+    cin>>roll2;
+    cout<<"Enter student 3 roll number: ";
+    cin>>roll3;
+    cout<<"Enter student 4 roll number: ";
+    cin>>roll4;
+    cout<<"Enter student 5 roll number: ";
+    cin>>roll5;
+
+    cout<<"The roll number student 1 is: "<<roll1<<"\n";
+    cout<<"The roll number student 2 is: "<<roll2<<"\n";
+    cout<<"The roll number student 3 is: "<<roll3<<"\n";
+    cout<<"The roll number student 4 is: "<<roll4<<"\n";
+    cout<<"The roll number student 5 is: "<<roll5<<"\n";
+
+    return 0;
+
+}
